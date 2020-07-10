16754ac763ee5c33d6a85421b526e3ab98334dd1
Hollis Jamison <hollisjamison@gmail.com>
2020-07-09 20:29:42 -0400

    Implements calculate function which calculates data from 3 strings

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 function calculate(numberOne, operation, numberTwo) {
+  // cleanse our data of undefined
+  const numberOneClean = isNaN(numberOne) ? 0 : numberOne
+  const numberTwoClean = isNaN(numberTwo) ? 0 : numberTwo
+
+  switch (operation) {
+    case '+':
+      return parseInt(numberOneClean) + parseInt(numberTwoClean)
+    case '-':
+      return parseInt(numberOneClean) - parseInt(numberTwoClean)
+    case '*':
+      return parseInt(numberOneClean) * parseInt(numberTwoClean)
+    case '/':
+      return parseInt(numberOneClean) / parseInt(numberTwoClean)
+    default:
+      return 0
+  }
 }
 
 /**
